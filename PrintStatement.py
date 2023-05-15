9187b38830d3332a2499bd8d7978ad784fe95e29
Dilip Kumar <126438193+dilipkm007@users.noreply.github.com>
2023-05-15 10:31:34 +0530

    Add files via upload

---
+++ PrintStatement.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 15 09:38:12 2023
+
+@author: kumardi
+"""
+
+print('7 + 3= ', 7 + 3)
+print('7 / 3 = ', 7/3)
+print('7 // 3 = ', 7//3)
+print('-7 / 3 = ', -7/3)
+print('-7 // 3 = ', -7//3)
+print('7 ** 3 = ', 7**3)
+print('Hello World!')
+print("How are\n\tyou today")
+print('How\'s it going')
+print("How's it going")
+print('one', end=' ')
+print('Two', end=' ')
+print('Three', end=' ')
+print('Four', end=' ')
+print('Five')
+
+world = 52
+print(world)
+
+
+#Challenge
+print("one, 'two', \"Three\", four, \\five\\, once, 'I' caught a fish '//alive\\\\'")
