352c7e78b982ba928232b82a6086ba2a4a830055
Dilip Kumar <126438193+dilipkm007@users.noreply.github.com>
2023-05-19 19:16:42 +0530

    Add files via upload

---
+++ PythonModules.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 18:11:27 2023
+
+@author: Dilip Kumar
+"""
+
+## Math Module
+import math
+
+print(math.pi)
+print(math.cos(0))
+print()
+
+## Random Module
+from random import randint
+for i in range(100):
+    print(randint(1, 100), end=' ')
+print()
+
+##WebBrowser Module
+import webbrowser as web
+web.open("http://www.dilipmaheshwari.com")
+
